ef3576b247ddf2b799741aaf1fcec5f3f2521549
Xin Hong <hongxinhope@gmail.com>
2016-07-27 13:35:51 +0800

    init repository

---
+++ Example/ExampleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleViewController.swift
+//  Example
+//
+//  Created by Xin Hong on 16/7/27.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import UIKit
+import ManagedObjectAdapter
+
+class ExampleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ ManagedObjectAdapter/ManagedObjectAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  ManagedObjectAdapter.swift
+//  ManagedObjectAdapter
+//
+//  Created by Xin Hong on 16/7/27.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import Foundation
